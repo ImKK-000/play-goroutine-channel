3fe9d726bbcda904aa3abe83ef8fddcf794ccf1d
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-12-10 17:56:51 +0700

    [Added]: initial files

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var channel = make(chan string, 10)
+var ports = []int{8000, 8001}
+
+type APIHandler struct{}
+
+func (apiHandler APIHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	channel <- r.Host
+	w.Write([]byte("done"))
+}
+
+func NewServer(port int) {
+	apiHandler := APIHandler{}
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), apiHandler)
+	log.Fatalln(err)
+}
+
+func main() {
+	go NewServer(ports[0])
+	go NewServer(ports[1])
+
+	for {
+		fmt.Println(<-channel)
+	}
+}
---
+++ request.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl http://127.0.0.1:8000/api
+curl http://127.0.0.1:8001/api
